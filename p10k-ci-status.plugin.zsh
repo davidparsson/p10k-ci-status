2872c221f144f82855e1a710362d7027cf17ab2f
David Pärsson <david@parsson.se>
2023-10-26 16:45:32 +0200

    Initial synchronous version

---
+++ p10k-ci-status.plugin.zsh
@@ -0,0 +1,52 @@
+# TODO:
+# - [ ] Asynchronous execution
+# - [ ] Fall-back: hub ci-status $(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
+# - [ ] Allow configuration
+# - [ ] Add a readme
+#
+
+function prompt_ci_status() {
+    local state color='' text=''
+    local output exit_code
+    local CHECK='✔︎' CROSS='✖︎' BULLET='•' TRIANGLE='▴'
+    
+    output="$(hub ci-status)"
+    exit_code=$?
+
+    case $exit_code in
+        0)
+            text="$CHECK"
+            if [[ $output == "neutral" ]]; then
+                state=NEUTRAL
+                color=gray
+            else
+                state=SUCCESS
+                color=green
+            fi
+            ;;
+        1)
+            text="$CROSS"
+            if [[ $output == "action_required" ]]; then
+                state=ACTION_REQUIRED
+                text="$TRIANGLE"
+                color=red
+            elif [[ $output == "cancelled" || $output == "timed_out" ]]; then
+                state=CANCELLED
+                color=yellow
+            elif [[ $output == "failure" || $output == "error" ]]; then
+                state=FAILURE
+                color=red
+            fi
+            ;;
+        2)
+            state=BUILDING
+            text="$BULLET"
+            color=yellow
+            ;;
+        3)
+            state=NO_STATUS
+            ;;
+    esac
+
+    p10k segment -s $state -f $color -t $text
+}
